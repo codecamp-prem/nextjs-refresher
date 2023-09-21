8568bed9095386fc0db5cfb82e032042a1eff4f7
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-21 16:07:42 +0545

    creating an object

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -8,3 +8,11 @@ export function GET(request: NextRequest) {
     { id: 2, name: "Jane Doe" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // validate the body, if not passed , return error
+  if (!body.name)
+    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 }); // 201 :
+}
