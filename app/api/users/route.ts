31c63109c101939c6ff4c2b71975f0eda1c54a85
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-21 17:24:13 +0545

    deleting an object

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -16,3 +16,17 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: "name is required" }, { status: 400 });
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 }); // 201 :
 }
+interface Props {
+  params: { id: number };
+}
+// updating
+export async function PUT(request: NextRequest, { params }: Props) {
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "name is required" }, { status: 400 });
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "user not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: body.name });
+}
