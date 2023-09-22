9c5acf7e47f369abf3bd5a640ace3320ff8d46d5
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-22 16:16:45 +0545

    configuring credential provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,10 +2,56 @@ import prisma from "@/prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import schema from "./schema";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@email.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) return null;
+
+        const validation = schema.safeParse(credentials);
+        // validate the credentials with Zod, if not passed , return error
+        if (!validation.success) return null;
+
+        // Add logic here to look up the user from the credentials supplied
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          const passwordMatch = await bcrypt.compare(
+            credentials.password,
+            user.hashedPassword!
+          );
+
+          return passwordMatch ? user : null;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
