a035d635773bf05392d2367303e4e9e3c361f3c0
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-23 06:23:42 +0545

    optimizing remote image auto. by next

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,17 @@ import Image from "next/image";
 
 export default async function Home() {
   return (
-    <main>
+    <main className="relative h-screen">
       <Image src={house} alt="Main Home" />
+      <Image
+        className="object-cover"
+        src="https://bit.ly/react-cover"
+        alt="Main Home"
+        fill
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
   );
 }
