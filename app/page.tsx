5cb203467dcfd6ea38fa6c4799d061d409f683de
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2023-09-23 06:11:37 +0545

    optimizing local image auto by next

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
+import house from "@/public/images/hilly-island.jpeg"; // next js auto optimise huge image into like .webp forma
 import Image from "next/image";
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
   return (
     <main>
-      <h1>herro {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">users</Link>
-      <ProductCard />
+      <Image src={house} alt="Main Home" />
     </main>
   );
 }
